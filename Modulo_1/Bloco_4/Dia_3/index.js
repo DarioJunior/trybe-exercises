57e43a3ec5e7dbfd6d8e8177938fccbaa21b9b47
Dario Junior <dario.junior.rs@gmail.com>
2021-04-05 18:35:30 -0300

    ainda meio perdido com esses commits bloco 5

---
+++ Modulo_1/Bloco_4/Dia_3/index.js
@@ -0,0 +1,26 @@
+let qtd = 5;
+let char = '*';
+let space = ' ';
+
+let pyramheight =
+
+  let arr = [];
+
+for (let line = 0; line <= qtd; line++) {
+  console.log(char.repeat(line))
+}
+for (let column = 0; column <= qtd; column++) {
+  console.log(space.repeat(line + 1), char.repeat(line))
+}
+
+// for (let i = 0; i <= line; i++) {
+//   exp = line - (i + 1);
+//   if (exp >= 0) {
+//     console.log(" ".repeat(exp), "*".repeat(i))
+//   }
+// }
+
+
+// for (let i = 0; i < line; i++) {
+//   console.log(' '.repeat(line - i), char.repeat(i + 1));
+// }
