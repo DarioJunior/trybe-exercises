ce3b55739d94cb641748bfa18f8bb8b059e03006
Dario Junior <dario.junior.rs@gmail.com>
2021-06-23 16:22:25 -0300

    Não haverá conteúdo aqui pois, o exercício será realizado em um fork de outro projeto

---
+++ exercises/testes-automatizados-com-react-testing-library_1/exercise_1.js
@@ -0,0 +1,7 @@
+/*
+Para realizar os exercícios será necessário utilizar um FORK dos repositórios 
+https://github.com/tryber/exercise-todo-list
+https://github.com/tryber/exercise-tic-tac-toe
+
+será realizado um push em branco!
+*/ 
